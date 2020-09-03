0044229b440ac7eb1a2e66f5e4c135b9e23e7c67
CamilaGO <gon18398@uvg.edu.gt>
2020-09-03 01:41:43 -0500

    Mejore el tono de snow

--- materials.py
+++ materials.py
@@ -12,7 +12,7 @@ class Material(object):
 
 ivory = Material(diffuse=color(100, 100, 80))
 rubber = Material(diffuse=color(80, 10, 0))
-snow = Material(diffuse=color(200, 200, 200))
+snow = Material(diffuse=color(222, 231, 236))
 button = Material(diffuse=color(0, 0, 0))
 eye = Material(diffuse=color(250, 250, 250))
 carrot = Material(diffuse=color(255, 165, 0))
